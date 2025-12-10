37972bbe1289b709154710b177788923bcca9293
JuridicalBear14 <throwawayemail2223@gmail.com>
2025-12-09 18:36:56 -0800

    Set up basic UI, needs gameplay functionality

--- game_manager.js
+++ game_manager.js
@@ -1,2 +1,43 @@
 // The actual game backend code, called by UI stuff
+let word = ""   // Solution word the user is looking for
+let current_row = 0   // Row we're currently on (aka how many guesses left)
 
+// Fetch total wordlist
+async function fetchWordList() {
+    const response = await fetch("./all-words.txt")
+    const data = await response.text()
+
+    // Now we have the data
+    let final = data.split("\n")
+    return final
+}
+
+// Fetch solution list
+async function fetchAnswerWordList() {
+    const response = await fetch("./answer-words.txt")
+    const data = await response.text()
+
+    // Now we have the data
+    let final = data.split("\n")
+    return final
+}
+
+// Get the game data ready
+async function loadData() {
+    let wordlist = await fetchWordList()
+    let awordlist = await fetchAnswerWordList()
+
+    return [wordlist, awordlist]
+}
+
+// Start and play the actual game
+function chooseWord() {
+    loadData().then(lists => {
+        const [wordlist, answerlist] = lists
+
+        let randomIndex = Math.floor(Math.random() * answerlist.length)
+        word = answerlist[randomIndex]
+    })
+}
+
+chooseWord()
---
+++ ui_builder.js
@@ -0,0 +1,76 @@
+// The code to place all of the boxes and stuff on screen
+
+// Initialize grid of letters
+function buildLetterGrid() {
+    // Build a 5x6 grid of letter-box divs, each one should have an id matching its position (ex: 1:1)
+    let main = document.getElementById("letter-container")
+
+    // Loop for rows
+    for (let i = 0; i < 6; i++) {
+        // Build row div
+        let r = document.createElement("div")
+        r.setAttribute("id", `${i}`)
+        r.setAttribute("class", "lb-row")
+
+        // Now create and add letter boxes
+        for (let j = 0; j < 5; j++) {
+            let b = document.createElement("div")
+            b.setAttribute("class", "letter-box")
+            b.setAttribute("id", `${i}${j}`)
+
+            r.appendChild(b)
+        }
+
+        main.appendChild(r)
+    }
+}
+
+// Initialize keyboard
+function buildKeyboard() {
+    // Define rows (easier to just do manually)
+    const rows = [
+        "qwertyuiop",
+        "asdfghjkl",
+        "1zxcvbnm2"   // 1 and 2 represent enter and delete respectively
+    ]
+
+    let main = document.getElementById("keyboard-container")
+
+    // Loop through rows
+    for (let i = 0; i < 3; i++) {
+        let r = document.createElement("div")
+        r.setAttribute("class", "kb-row")
+        r.setAttribute("id", `${i}`)
+
+        // Now create letters
+        for (let j = 0; j < rows[i].length; j++) {
+            let name = rows[i][j]
+
+            if (name == "1") {
+                name = "enter"
+            } else if (name == "2") {
+                name = "\u2190"
+            }
+
+            let b = document.createElement("div")
+            b.setAttribute("class", "keyboard-key")
+            b.setAttribute("id", `${name}`)
+            b.innerHTML = name
+
+            // Make the back arrow a little bigger
+            if (name == "\u2190") {
+                b.style.fontSize = 30
+            }
+
+            // TODO: give each one some js code to call corresponding function when clicked
+
+            r.appendChild(b)
+        }
+
+        main.appendChild(r)
+    }
+}
+
+// On startup build the ui
+buildLetterGrid()
+buildKeyboard()
